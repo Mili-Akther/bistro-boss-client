3973fec6fc3717c7153be50a2609acc582b32d2a
Mili Akther <miliakthermilu@gmail.com>
2025-06-19 16:50:24 +0600

    Project setup and folder structure done

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+
+
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <h2>Route not Found</h2>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        
+      },
+    ],
+  },
+]);
+
+export default routes;
