9dff6d6e71313c9d91e1e752a0551246b0faee46
Mili Akther <miliakthermilu@gmail.com>
2025-07-04 12:46:28 +0600

    Install stripe, stripe for react and create card element

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
+// TODO: add publishable key
+const stripePromise = loadStripe('')
 const Payment = () => {
       return (
             <div>
                   <SectionTitle heading='Payment' subHeading="Please pay to eat"></SectionTitle>
                   <div>
-                        <h2 className='text-4xl'>Money come to me </h2>
+                        <Elements stripe={stripePromise}></Elements>
                   </div>
             </div>
       );
